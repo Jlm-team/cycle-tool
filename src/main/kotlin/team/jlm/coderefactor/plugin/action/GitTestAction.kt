82bc98fb32a85e0a92232c4c169ed8ca79881d97
XYZboom <3242304316@qq.com>
2022-06-17 18:59:06 +0800

    feature(actions): 新增测试用action

---
+++ src/main/kotlin/team/jlm/coderefactor/plugin/action/GitTestAction.kt
@@ -0,0 +1,11 @@
+package team.jlm.coderefactor.plugin.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GitTestAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        // TODO 在这里写一些按钮按下去触发的内容
+    }
+}
