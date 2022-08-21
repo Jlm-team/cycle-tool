fa13dcda372c0aa00e834f8e50d105f5e2a43a28
Nicholas_Alex <alex@njust.edu.cn>
2022-08-21 14:32:02 +0800

    Vue

--- micro_service/src/main/kotlin/team/jlm/controller/GraphController.kt
+++ micro_service/src/main/kotlin/team/jlm/controller/GraphController.kt
@@ -1,17 +1,23 @@
 package team.jlm.controller
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.PathVariable
 import team.jlm.entity.GraphBean
 import team.jlm.utils.packJson
+import java.io.IOException
 
 @Controller("/")
 class GraphController {
     @Get("/{path}")
-    fun showGraph(@PathVariable path: String):HttpResponse<ArrayList<GraphBean>>{
-        val graphs = packJson(path)
+    fun showGraph(@PathVariable path: String): HttpResponse<ArrayList<GraphBean>> {
+        var graphs = ArrayList<GraphBean>()
+        try {
+            graphs = packJson(path)
+        } catch (e: IOException) {
+            return HttpResponse.created(graphs)
+        }
         return HttpResponse.created(graphs)
     }
 }
